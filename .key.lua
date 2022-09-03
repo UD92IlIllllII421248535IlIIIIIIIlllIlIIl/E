6561c9622a59b510a51b118cbb43e0d60669f2a0
UD92IlIllllII421248535IlIIIIIIIlllIlIIl <110747829+UD92IlIllllII421248535IlIIIIIIIlllIlIIl@users.noreply.github.com>
2022-09-03 09:39:31 -0700

    Update .key.lua

--- .key.lua
+++ .key.lua
@@ -27,7 +27,50 @@ while true do
     if _G.Key == keyCheck then
         --Not spoofed
     else
-        game.Players.LocalPlayer:Kick("Do not try and spoof your key!")
+        game.Players.LocalPlayer:Kick("Script detected: Attenmpt to spoofed. Username and exploit logged.")
+        
+        local webhookcheck =
+is_sirhurt_closure and "Sirhurt" or pebc_execute and "ProtoSmasher" or syn and "Synapse X" or
+secure_load and "Sentinel" or
+KRNL_LOADED and "Krnl" or
+SONA_LOADED and "Sona" or
+"Unknown exploit"
+
+
+
+local url =
+"https://discord.com/api/webhooks/1015661562851241984/KVk4w-B67RmzO3KfSDhG31I5nkFhHe1z17ZP88vNZTIxX9e1cw7KsnZEJd-XGVPrJZwJ"
+local data = {
+["content"] = "**User key spoofed/denied/failed:**",
+["embeds"] = {
+{
+["title"] =  "Username: " .. game.Players.LocalPlayer.Name..". Exploit: "..webhookcheck..""..". GUI: VIP",
+["type"] = "rich",
+["color"] = tonumber(0),
+["image"] = {
+["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=" ..
+tostring(game:GetService("Players").LocalPlayer.Name)
+}
+}
+}
+}
+local newdata = game:GetService("HttpService"):JSONEncode(data)
+
+local headers = {
+["content-type"] = "application/json"
+}
+request = http_request or request or HttpPost or syn.request
+local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+request(abcdef)
+
+local denied_message = "Denied."
+local g
+g = hookfunction(print,function(urprint,...)
+if string.match(urprint, "HTTP") or string.match(urprint, "http") or string.match(urprint, ".com") or string.match(urprint, "Http") or string.match(urprint, "Paste") or string.match(urprint, "bin") or string.match(urprint, "gitusercontent") or string.match(urprint, "git") then
+urprint = denied_message
+end
+return g(urprint,...)
+end)
     end
     wait()
 end
